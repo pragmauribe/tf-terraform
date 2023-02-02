d9da6008eb90212e5ff33df1e5115a10f067b467
Ivancho <117919901+pragmauribe@users.noreply.github.com>
2023-02-02 10:16:57 -0500

    adding backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+  backend "s3" {
+    bucket = "tf-azure-terraform-state"
+    key    = "tfstate.tf"
+    region = "us-west-2"
   }
 }
 
